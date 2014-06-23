e3cef80fe36f52e95e66fc98d093ffdb5f2c2ab6
Mario Aquino <mario.e.aquino@gmail.com>
2014-06-22 20:54:15 -0500

    Added first test!

---
+++ test/prisoners_dilemma_clj/play_test.clj
@@ -0,0 +1,10 @@
+(ns prisoners-dilemma-clj.play-test
+  (use midje.sweet)
+  (:require [clojure.test :refer :all]
+            [prisoners-dilemma-clj.play :refer :all]))
+
+(fact "scoring a grid yields a map of names and points"
+      (let [sucker-strategy "pretend I am a sucker"
+            named-strategy (with-meta 'sucker-strategy {:name "sucker"})
+            my-grid [{:points 10 :strategy named-strategy}]]
+        (score-grid my-grid) => '({:name "sucker" :points 10})))
--- test/prisoners_dilemma_clj/strategies_test.clj
+++
@@ -1,7 +0,0 @@
-(ns prisoners-dilemma-clj.strategies-test
-  (:require [clojure.test :refer :all]
-            [prisoners-dilemma-clj.strategies :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
